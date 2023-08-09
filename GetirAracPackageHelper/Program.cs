4e27aab7bd0734b11f72e784ca39230394b793e5
Egemen Çiftci <egemenciftci@gmail.com>
2023-08-09 22:15:00 +0300

    Table added.

--- GetirAracPackageHelper/Program.cs
+++ GetirAracPackageHelper/Program.cs
@@ -1,6 +1,7 @@
 ﻿using GetirAracPackageHelper;
 using System.Text.Json;
 
+const int TableWidth = 73;
 Car[]? cars = InitializeCars();
 
 if (cars == null || cars.Length <= 0)
@@ -9,55 +10,96 @@ if (cars == null || cars.Length <= 0)
     return;
 }
 
-cars.Select((car, i) => $"{i}. {car.PricePerMinute}TL/min range car").ToList().ForEach(Console.WriteLine);
-int carIndex = -1;
-while (carIndex < 0 || carIndex >= cars.Length)
+while (true)
 {
-    Console.Write("Please select a car: ");
-    carIndex = Convert.ToInt32(Console.ReadLine());
+    cars.Select((car, i) => $"{i}. {car.PricePerMinute}TL/min range car").ToList().ForEach(Console.WriteLine);
+    int carIndex = -1;
+    while (carIndex < 0 || carIndex >= cars.Length)
+    {
+        Console.Write("Please select a car: ");
+        carIndex = Convert.ToInt32(Console.ReadLine());
+    }
+
+    int estimatedKms = -1;
+    while (estimatedKms < 0)
+    {
+        Console.Write("Estimated distance in kilometers: ");
+        estimatedKms = Convert.ToInt32(Console.ReadLine());
+    }
+
+    int estimatedMinutes = -1;
+    while (estimatedMinutes < 0)
+    {
+        Console.Write("Estimated duration in minutes: ");
+        estimatedMinutes = Convert.ToInt32(Console.ReadLine());
+    }
+
+    decimal discount = -1;
+    while (discount < 0)
+    {
+        Console.Write("Discount in TL: ");
+        discount = Convert.ToInt32(Console.ReadLine());
+    }
+
+    List<(Package package, decimal totalPrice)> totalPrices = new();
+
+    for (int i = 0; i < cars[carIndex].Packages.Length; i++)
+    {
+        Package package = cars[carIndex].Packages[i];
+        int minutesToPay = Math.Max(estimatedMinutes - package.FreeMinutes, 0);
+        int kilometersToPay = Math.Max(estimatedKms - package.FreeKilometers, 0);
+        decimal totalPrice = (minutesToPay * cars[carIndex].PricePerMinute) + (kilometersToPay * cars[carIndex].PricePerKilometer) + package.Price - discount;
+        totalPrices.Add((package, totalPrice));
+    }
+
+    Console.WriteLine($"\nThe cheapest top 3 packages for {cars[carIndex].PricePerMinute}TL/min range car, {estimatedKms}km, {estimatedMinutes}min, {discount}TL are:");
+    DrawTable(totalPrices.OrderBy(f => f.totalPrice).Take(3));
+
+    Console.WriteLine("\nPress any key to calculate again...\n");
+    _ = Console.ReadKey();
 }
 
-int estimatedKms = -1;
-while (estimatedKms < 0)
+static void DrawTable(IEnumerable<(Package package, decimal totalPrice)> items)
 {
-    Console.Write("Estimated distance in kilometers: ");
-    estimatedKms = Convert.ToInt32(Console.ReadLine());
+    PrintLine();
+    PrintRow("Total Price", "Free Mins", "Free Kms", "Price");
+    PrintLine();
+    foreach ((Package package, decimal totalPrice) in items)
+    {
+        PrintRow($"{totalPrice}TL", $"{package.FreeMinutes}min", $"{package.FreeKilometers}km", $"{package.Price}TL{(package.FreeMinutes == 0 ? " (No package)" : null)}");
+    }
+
+    PrintLine();
 }
-
-int estimatedMinutes = -1;
-while (estimatedMinutes < 0)
-{
-    Console.Write("Estimated duration in minutes: ");
-    estimatedMinutes = Convert.ToInt32(Console.ReadLine());
-}
-
-decimal discount = -1;
-while (discount < 0)
-{
-    Console.Write("Discount in TL: ");
-    discount = Convert.ToInt32(Console.ReadLine());
-}
-
-List<(Package package, decimal totalPrice)> totalPrices = new();
-
-for (int i = 0; i < cars[carIndex].Packages.Length; i++)
-{
-    Package package = cars[carIndex].Packages[i];
-    int minutesToPay = Math.Max(estimatedMinutes - package.FreeMinutes, 0);
-    int kilometersToPay = Math.Max(estimatedKms - package.FreeKilometers, 0);
-    decimal totalPrice = (minutesToPay * cars[carIndex].PricePerMinute) + (kilometersToPay * cars[carIndex].PricePerKilometer) + package.Price - discount;
-    totalPrices.Add((package, totalPrice));
-}
-
-Console.WriteLine("");
-Console.WriteLine($"The cheapest top 3 packages for {cars[carIndex].PricePerMinute}TL/min range car, {estimatedKms}km, {estimatedMinutes}min, {discount}TL are:");
-totalPrices.OrderBy(f => f.totalPrice).Take(3).Select(f => $"{f.totalPrice}TL\t=> Free Mins:{f.package.FreeMinutes}min\tFree Kms:{f.package.FreeKilometers}km\tPrice:{f.package.Price}TL{(f.package.FreeMinutes == 0 ? "\t(No package)" : null)}").ToList().ForEach(Console.WriteLine);
-
-Console.ReadKey();
 
 static Car[]? InitializeCars()
 {
     string jsonFilePath = "cars.json";
     string jsonContent = File.ReadAllText(jsonFilePath);
     return JsonSerializer.Deserialize<Car[]?>(jsonContent);
+}
+
+static void PrintLine()
+{
+    Console.WriteLine(new string('-', TableWidth));
+}
+
+static void PrintRow(params string[] columns)
+{
+    int width = (TableWidth - columns.Length) / columns.Length;
+    string row = "|";
+
+    foreach (string column in columns)
+    {
+        row += AlignCentre(column, width) + "|";
+    }
+
+    Console.WriteLine(row);
+}
+
+static string AlignCentre(string text, int width)
+{
+    text = text.Length > width ? text[..(width - 3)] + "..." : text;
+
+    return string.IsNullOrEmpty(text) ? new string(' ', width) : text.PadRight(width - ((width - text.Length) / 2)).PadLeft(width);
 }
